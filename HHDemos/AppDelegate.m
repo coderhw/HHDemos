8dd3873923379b8a087d307655702773dc33b318
coderhw <>
2020-06-03 23:05:43 +0800

    加入推送

--- HHDemos/AppDelegate.m
+++ HHDemos/AppDelegate.m
@@ -9,8 +9,9 @@
 #import "AppDelegate.h"
 #import "HHTabBarController.h"
 #import <objc/objc.h>
+#import <UserNotifications/UserNotifications.h>
 
-@interface AppDelegate ()
+@interface AppDelegate ()<UNUserNotificationCenterDelegate>
 
 @end
 
@@ -22,10 +23,12 @@
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     HHTabBarController  *rootVC = [[HHTabBarController alloc] init];
     
-    BOOL flag1 = YES;
-    BOOL flag2 = YES;
-    BOOL resultFlag = flag1 ^ flag2;
-    NSLog(@"resultFlag:%d", resultFlag);
+//    BOOL flag1 = YES;
+//    BOOL flag2 = YES;
+//    BOOL resultFlag = flag1 ^ flag2;
+//    NSLog(@"resultFlag:%d", resultFlag);
+    
+    [self registerAPNSService:application];
     
     self.window.rootViewController = rootVC;
     [self.window makeKeyAndVisible];
@@ -35,6 +38,49 @@
 - (void)applicationWillResignActive:(UIApplication *)application {}
 - (void)applicationWillEnterForeground:(UIApplication *)application {}
 - (void)applicationWillTerminate:(UIApplication *)application {}
+
+
+#pragma mark - APNS
+- (void)registerAPNSService:(UIApplication *)application {
+    
+    if(IOS10){
+        UNUserNotificationCenter * center = [UNUserNotificationCenter currentNotificationCenter];
+
+        [center setDelegate:self];
+        UNAuthorizationOptions type = UNAuthorizationOptionBadge|UNAuthorizationOptionSound|UNAuthorizationOptionAlert;
+        [center requestAuthorizationWithOptions:type completionHandler:^(BOOL granted, NSError * _Nullable error) {
+            if(granted) {
+                NSLog(@"注册成功");
+            }else{
+                NSLog(@"注册失败");
+            }
+        }];
+    }
+    
+    if(IOS8_10){
+        UIUserNotificationType notificationTypes = UIUserNotificationTypeBadge|UIUserNotificationTypeSound| UIUserNotificationTypeAlert;
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:notificationTypes categories:nil];
+        [application registerUserNotificationSettings:settings];
+    }
+}
+
+// iOS8+需要使用这个方法
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings {
+    // 检查当前用户是否允许通知,如果允许就调用 registerForRemoteNotifications
+    if (notificationSettings.types != UIUserNotificationTypeNone) {
+        [application registerForRemoteNotifications];
+    }
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+
+    NSString *deviceTokenStr = [[[[deviceToken description] stringByReplacingOccurrencesOfString:@"<" withString:@""] stringByReplacingOccurrencesOfString:@">" withString:@""] stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSLog(@"deviceTokenStr:\n%@",deviceTokenStr);
+
+}
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error{
+    NSLog(@"error -- %@",error);
+}
 
 
 @end
